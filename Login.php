f38a12c53fe3c7360f11629103b5dbb9b45910de
Tshehlana <tshehlanamonyebodi@gmail.com>
2024-03-05 09:16:47 +0200

    Log in page

---
+++ Login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loginstyle.css">
+    <title>Log In</title>
+</head>
+<body>
+    <div class="container">
+        <div class="picture">
+            <h2>Hello, Welome Back</h2>
+        </div>
+        <div class="log">
+
+        <h1 class="title">THE ELEMENT.</h1>
+        <h6 class="subtitle">STiMULATE YOUR MIND</h6>
+        <hr>
+            <h2>Log In </h2>
+
+            <div class="form">
+                <form action="" method="post">
+                    <div class="form-col">
+                        <label for="username">Username/ Email</label>
+                        <input type="text" name="username" id="username" placeholder="Enter Username">
+                    </div>
+                        
+                    <div class="form-col">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Enter Password">
+                    </div> 
+                    <a href="" class="forgot">Forgot Password</a>
+
+
+                    <input id="submit" type="submit" value="Log In" class="btn">
+                </form>
+
+
+                <p class="signup">Don't have an account? <a href="signup.php" >Sign Up Here!</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
